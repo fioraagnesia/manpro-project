be79e420ffd2f7283a18eb0bdd491354a7244f8a
fioraagnesia <142294637+fioraagnesia@users.noreply.github.com>
2025-10-27 22:37:12 +0700

    cleaning+merge hotel

--- cleaning-hotel.py
+++ cleaning-hotel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import warnings
+import re
 
 warnings.filterwarnings("ignore")
 
@@ -14,9 +15,10 @@ def clean_hotel_star(df, dataset_name, column_name='Hotel Star'):
     df.loc[~df[column_name].isin(valid_stars), column_name] = np.nan
 
     # Fill the null values of "Hotel Star" with the mode value
-    if not df[column_name].isnull().all(): # Check if the column is completely empty
-        mode_star = df[column_name].mode()[0]   # find the mode value of Hotel Star
-        df[column_name] = df[column_name].fillna(mode_star)
+    if not df[column_name].isnull().all():
+        # Replace null values with the mode value of Hotel Star
+        mode_star = df[column_name].mode()[0] 
+        df[column_name] = df[column_name].fillna(mode_star)     
         # Change the data type to integer
         df[column_name] = df[column_name].astype(int)
         
@@ -50,7 +52,7 @@ def clean_guest_rating(df, dataset_name, column_name='Guest Rating'):
     return df
 
 
-# FUNCTION for converting date to dd/mm/yyyy format
+# FUNCTION for converting date
 def date_format(df, dataset_name):
     print(f"--- Formatting dates for '{dataset_name}' to dd/mm/yyyy ---")
     
@@ -62,12 +64,23 @@ def date_format(df, dataset_name):
     df.dropna(subset=['Checkin Date', 'Checkout Date'], inplace=True)
 
     # 3. Change the date format to dd/mm/yyyy
-    if 'Checkin Date' in df.columns:
-        df['Checkin Date'] = df['Checkin Date'].dt.strftime('%d/%m/%Y')
-    if 'Checkout Date' in df.columns:
-        df['Checkout Date'] = df['Checkout Date'].dt.strftime('%d/%m/%Y')
+    # if 'Checkin Date' in df.columns:
+    #     df['Checkin Date'] = df['Checkin Date'].dt.strftime('%d/%m/%Y')
+    # if 'Checkout Date' in df.columns:
+    #     df['Checkout Date'] = df['Checkout Date'].dt.strftime('%d/%m/%Y')
 
     return df
+
+# FUNCTION for normalizing name
+def normalize_name(name):
+    name = str(name).lower()
+    name = re.sub(r'[^\w\s]', '', name)
+    stop_words = ['hotel', 'resort', 'inn', 'guesthouse']
+    words = name.split()
+    filtered_words = [word for word in words if word not in stop_words]
+    filtered_words.sort()
+    name = " ".join(filtered_words).strip()
+    return name
 
 
 # DATA SCRAPING PATHS
@@ -118,3 +131,30 @@ for name, df in cleaned_df.items():
 for name, df in cleaned_df.items():
     print(f"Hotels listed on {name.title()}:")
     print(df)
+
+# Merge data from all platforms
+print("\n--- Merge data from all platforms... ---")
+df_all_multi = pd.concat(cleaned_df)
+df_all = df_all_multi.reset_index()
+df_all = df_all.rename(columns={'level_0': 'Platform'})
+df_all = df_all.drop(columns=['level_1'], errors='ignore') 
+
+# Skip if the price is below 100.000 (considered not valid)
+# df_all = df_all[df_all['Price'] > 100000].copy() 
+
+# Normalize the hotel names
+print("--- Normalizing hotel names... ---")
+df_all['Cleaned Name'] = df_all['Hotel Name'].apply(normalize_name)
+
+# Find the best price for a hotel from across the platforms (deduplication)
+print(f"--- Finding the best price for each hotel... ---")
+# 1. Sort price by ascending to get the cheapest price
+df_sorted = df_all.sort_values(by='Price', ascending=True)
+# 2. Compare the unique columns
+unique_cols = ['Cleaned Name', 'City', 'Country', 'Checkin Date', 'Checkout Date'] 
+# 3. Remove duplicates (with the same unique cols) and save the cheapest price
+df_best_price = df_sorted.drop_duplicates(subset=unique_cols, keep='first')
+
+print(f"Data after deduplication: {len(df_best_price)} rows")
+# Save as csv
+df_best_price.to_csv('hotel_data_combined.csv', index=False)
